e6ac7e989bb51afb788951930d0102d35b312a6a
Alexey Parfenov <alepar@gmail.com>
2016-10-13 19:13:19 -0700

    diwali lights (lazy) - https://www.hackerrank.com/challenges/diwali-lights

---
+++ src/challenges/diwalilights/Solution.java
@@ -0,0 +1,29 @@
+package challenges.diwalilights;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        final BigInteger two = new BigInteger("2");
+
+        try(Scanner stdin = new Scanner(System.in)) {
+            final int T = stdin.nextInt();
+            for (int t=0; t<T; t++) {
+                final int N = stdin.nextInt();
+
+                final BigInteger answer = two.pow(N);
+                String answerString = answer.toString();
+                answerString = answerString.substring(Math.max(0, answerString.length()-5), answerString.length());
+
+                int answerInteger = Integer.valueOf(answerString);
+                if (answerInteger == 0) {
+                    answerInteger = 99999;
+                } else {
+                    answerInteger-=1;
+                }
+                System.out.println(answerInteger);
+            }
+        }
+    }
+}
