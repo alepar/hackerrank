9882d1c627253e0d30397ef8c8f023b470298404
Alexey Parfenov <parfenov@amazon.com>
2017-03-03 14:24:55 -0800

    bomber-man updated to 'fast'

--- src/algorithms/implementation/bomber_man/Solution.java
+++ src/algorithms/implementation/bomber_man/Solution.java
@@ -1,5 +1,8 @@
 package algorithms.implementation.bomber_man;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
@@ -10,29 +13,31 @@ public class Solution {
             final long N = stdin.nextLong();
             stdin.nextLine();
 
-            int field[][] = new int[R][];
+            int field[] = new int[R*C];
             for (int r=0; r<R; r++) {
-                field[r] = new int[C];
-
                 final String line = stdin.nextLine();
                 for (int c=0; c<C; c++) {
                     if (line.charAt(c) != '.') {
-                        field[r][c] = 3;
+                        field[r*C+c] = 3;
                     }
                 }
             }
 
-            for (long i = 0; i <= N; i++) {
+            final List<int[]> fields = new ArrayList<>();
+            int startCycle = -1;
+
+            long i = 0;
+            iter: for (; i <= N; i++) {
                 for (int r = 0; r < R; r++) {
                     for (int c = 0; c < C; c++) {
 
-                        final int cell = field[r][c];
+                        final int cell = field[r*C+c];
                         if (i % 2 == 0) {
                             if (i > 0) {
                                 if (cell == 0) {
-                                    field[r][c] = 3;
+                                    field[r*C+c] = 3;
                                 } else {
-                                    field[r][c]--;
+                                    field[r*C+c]--;
                                 }
                             }
                         } else {
@@ -44,30 +49,41 @@ public class Solution {
                                         final int nc = c + (1-j/2)*d;
 
                                         if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
-                                            if (field[nr][nc] > 1) {
-                                                field[nr][nc] = 0;
+                                            if (field[nr*C+nc] > 1) {
+                                                field[nr*C+nc] = 0;
                                             }
                                         }
                                     }
                                 }
-                                field[r][c]--;
+                                field[r*C+c]--;
                             }
                         }
 
                     }
                 }
 
+                fields.add(Arrays.copyOf(field, field.length));
+                for (int d=(int)(i%2); d<i; d+=2) {
+                    if (Arrays.equals(fields.get(d), field)) {
+                        startCycle = d;
+                        break iter;
+                    }
+                }
             }
 
-            printField(R, C, field);
+            if (i != N) {
+                i = (N-startCycle) % (i-startCycle) + startCycle;
+            }
+
+            printField(R, C, fields.get((int)i));
             System.out.println();
         }
     }
 
-    private static void printField(int r, int c, int[][] field) {
+    private static void printField(int r, int c, int[] field) {
         for (int ir = 0; ir < r; ir++) {
             for (int ic = 0; ic < c; ic++) {
-                if(field[ir][ic]>0) {
+                if(field[ir*c+ic]>0) {
                     System.out.print('O');
                 } else {
                     System.out.print('.');
