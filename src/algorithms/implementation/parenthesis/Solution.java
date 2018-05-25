417de106e967a06ffade9136242a7bf52e958897
Alexey Parfenov <parfenov@amazon.com>
2018-05-25 10:14:09 -0700

    https://leetcode.com/problems/longest-valid-parentheses/description/

--- src/algorithms/implementation/parenthesis/Solution.java
+++ src/algorithms/implementation/parenthesis/Solution.java
@@ -1,5 +1,10 @@
+package algorithms.implementation.parenthesis;
+
 import java.util.Scanner;
 
+/*
+* https://leetcode.com/problems/longest-valid-parentheses/description/
+*/
 public class Solution {
     public static void main(String[] args) {
         try(Scanner stdin = new Scanner(System.in)) {
